aae32fec4f088cc3a6163f963f40b4da87161ea8
Irbis74 <42510936+Irbis74@users.noreply.github.com>
2021-02-13 10:51:43 +0300

    DmitryT74.task.maven

--- src/main/java/DmitryT74/task/Application/Application.java
+++ src/main/java/DmitryT74/task/Application/Application.java
@@ -2,7 +2,7 @@ package DmitryT74.task.Application;
 
 public class Application {
     public static void main(String[] args) {
-        OneTask tsk=new OneTask("Sa","Hig","T_One","2021-02-18","2021-02-07","What is it?");
+        OneTask tsk=new OneTask("Sa","Hig","T_One","2021-02-26","2021-02-07","Test drive");
         //example
         System.out.println("Hurry up , you only have left  "+tsk.getDayToDo()+ " days");
     }
